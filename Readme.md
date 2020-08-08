ca243a5da41f99f39e10a896339cd52287e3e726
Aldric Pereira <52449719+A1dricAP@users.noreply.github.com>
2020-08-08 22:35:42 +0530

    2

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Social App
+
+## Django framework and React
+
+This app is a social media categorised one. The main purpose is to learn Django framework.
+
+You need to create separate apps for the parts of the project. There is a blog app that consists of `views.py` and `urls.py`.
+
+The `urls.py` file, basically is used to map the urls to the corresponding function in `view.py`.
+
+---
+
+There exists a `templates` folder within the `blog` folder; responsible for mapping out the results to be displayed to the user, via the `urls.py`.
+
+---
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,28 @@
 # this file to define what has to be sent to the user.
 
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [{
+    'name': 'Aldric Pereira',
+    'title': 'Don\'t really know',
+    'content': 'First post',
+    'date_posted': 'August 08, 2020'
+},
+    {
+    'name': 'Aaron',
+    'title': 'i really know',
+    'content': 'second post',
+    'date_posted': 'August 18, 2020'
+}
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
