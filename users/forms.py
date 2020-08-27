19882277988f4dc05b8ca39571ab14013a702db3
Aldric Pereira <52449719+A1dricAP@users.noreply.github.com>
2020-08-27 23:00:40 +0530

    Update User Profile

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 # ! inheriting `UserCreationForm` into `UserRegistrationForm`
 
@@ -11,3 +12,21 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+# this form will allow us to update our username and email.
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+# this form will allow us to update our profile image.
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
