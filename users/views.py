3e7321ba9953307c9c6f411e4176f738282cd8f9
Aldric Pereira <52449719+A1dricAP@users.noreply.github.com>
2020-09-02 17:35:25 +0530

    fixed bugs to correct errors - in Social/user/models.py

--- users/views.py
+++ users/views.py
@@ -6,11 +6,11 @@ from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 # Create your views here.
 
 
-def register(request):
+def register(request, *args, **kwargs):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
+            form.save(*args, **kwargs)
             username = form.cleaned_data.get('username')
             messages.success(
                 request, f'Your account has been created! You can now login!')
@@ -23,19 +23,19 @@ def register(request):
 
 
 @login_required
-def profile(request):
+def profile(request, *args, **kwargs):
     if request.method == 'POST':
         # here instance is basically setting the default input into the fields.
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(
             request.POST, request.FILES, instance=request.user.profile)
         if u_form.is_valid() and p_form.is_valid():
-            u_form.save(force_insert=False)
-            p_form.save(force_insert=False)
+            u_form.save(*args, **kwargs)
+            p_form.save(*args, **kwargs)
             messages.success(
                 request, f'Your account has been updated, mate!')
             # * redirecting to the blog homepage, using the name given for the url pattern of blog homepage.
-            return redirect('profile')
+            return redirect('blog-home')
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
